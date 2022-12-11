72b5bb9663adff0a14ba219b747b281ce3a6e2a7
Anujay Kumar <ak.anujay@gmail.com>
2022-12-12 00:52:39 +0530

    env

--- src/components/firebase-config.tsx
+++ src/components/firebase-config.tsx
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
+console.log('process.env.REACT_APP_FIREBASE_API_KEY',process.env.REACT_APP_FIREBASE_API_KEY)
 const firebaseConfig = {
-  apiKey: "AIzaSyCf3y5jfy0x_tLkBpzQuTffapqYznYj56s",
-  authDomain: "react-firebase-c43f0.firebaseapp.com",
-  projectId: "react-firebase-c43f0",
-  storageBucket: "react-firebase-c43f0.appspot.com",
-  messagingSenderId: "987498930071",
-  appId: "1:987498930071:web:3186d4d5b18b8273616ed2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
